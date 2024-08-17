2d45fcff61c6338b6ed4edabf16ff6322f3ffd46
RITIK SHARMA <ritiksharma555598@gmail.com>
2024-08-17 11:19:54 +0530

    lat commet

--- project/news/app/src/main/java/com/example/news/AdminLoginActivity.java
+++ project/news/app/src/main/java/com/example/news/AdminLoginActivity.java
@@ -1,22 +1,11 @@
 package com.example.news;
-
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class AdminLoginActivity extends AppCompatActivity {
--- project/news/app/src/main/java/com/example/news/AdminRegistrationActivity.java
+++ project/news/app/src/main/java/com/example/news/AdminRegistrationActivity.java
@@ -1,20 +1,13 @@
 package com.example.news;
 
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
